3bcb87015353fe9469438aff6aea1b714662c5d6
radie <Radievskiy2003@gmail.com>
2023-09-16 03:19:04 +0300

    Initial commit

---
+++ src/main/java/com/example/minelandanalyticsspring/MinelandAnalyticsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.minelandanalyticsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinelandAnalyticsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinelandAnalyticsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/minelandanalyticsspring/MinelandAnalyticsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minelandanalyticsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinelandAnalyticsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
